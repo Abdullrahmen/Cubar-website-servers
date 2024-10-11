700760de8a520ce53dfbd221395f5fe0be7c44ac
Abdullrahmen <abdullrahmen2003@gmail.com>
2024-10-11 15:35:27 +0300

    deployment of web static functions

---
+++ deploy_web_static.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Fabric script that creates and distributes an archive web_static
+to the web servers
+"""
+
+from fabric.api import env, local, put, run, Connection
+from datetime import datetime
+from os.path import exists, isdir
+
+#List of servers' ips
+serverIP = '44.193.214.147'
+privateKeyPath = "/home/vagrant/Cubar/Keys/cubar-first-server.pem"
+env.hosts = ['44.193.214.147']
+
+def do_pack():
+    """generates a tgz archive from web_static folder to versions folder"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
+    
+
+def do_deploy(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        #path of the releases in the servers
+        path = "/data/web_static/releases/"
+        with Connection(f"ubuntu@{serverIP}", connect_kwargs={"key_filename": privateKeyPath}) as c:
+            c.put(archive_path, '/tmp/')
+            c.run('mkdir -p {}{}/'.format(path, no_ext))
+            c.run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+            c.run('rm /tmp/{}'.format(file_n))
+            c.run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+            c.run('rm -rf {}{}/web_static'.format(path, no_ext))
+            c.run('rm -rf /data/web_static/current')
+            c.run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
+
+def deploy():
+    """creates and distributes an archive to the web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
